f72869047f9041db51c3319fc096f7614d715391
AlexSteckler <steckler-alex@t-online.de>
2021-02-03 22:01:13 +0100

    Create Block Test

--- BlockSound/Assets/Cube.cs
+++ BlockSound/Assets/Cube.cs
@@ -7,7 +7,8 @@ public class Cube : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject cubeOne = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cubeOne.transform.position = new Vector3(0, 0.5f, 0);
     }
 
     // Update is called once per frame
